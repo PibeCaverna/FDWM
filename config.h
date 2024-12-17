481823489ef6e0a2f0ce2a44381c6d28cef8e020
Franco Aniello Falconieri <francoafalconieri@gmail.com>
2024-12-17 17:51:45 -0300

    changed font and highlighted background

--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "monospace:size=12" };
+static const char dmenufont[]       = "monospace:size=12";
 
 //Commented out in case I still need it
 //static const char col_gray1[]       = "#222222";
@@ -19,8 +19,8 @@ static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 //	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 }
 //	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  }
-	[SchemeNorm] = { light0, dark0, faded_green },
-	[SchemeSel]  = { light0_hard, dark0_hard,  bright_orange  },
+	[SchemeNorm] = { light0, dark0_hard, faded_green },
+	[SchemeSel]  = { light0_hard, faded_green,  bright_orange  },
 };
 
 /* tagging */

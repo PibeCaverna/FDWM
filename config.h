62f2df2dbd6e787049fa04caa3e8b1fee4af35d5
Franco Aniello Falconieri <francoafalconieri@gmail.com>
2025-02-04 16:32:53 -0300

    font shennanigans

--- config.h
+++ config.h
@@ -8,8 +8,8 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int vertpad            = 5;       /* vertical padding of bar */
 static const int sidepad            = 5;       /* horizontal padding of bar */
-static const char *fonts[]          = { "monospace:size=12" };
-static const char dmenufont[]       = "monospace:size=12";
+static const char *fonts[]          = { "Hack Nerd Font:size=14" };
+static const char dmenufont[]       = "Hack Nerd Font:size=14";
 
 //Commented out in case I still need it
 //static const char col_gray1[]       = "#222222";
